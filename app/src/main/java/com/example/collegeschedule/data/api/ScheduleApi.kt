e5bbbca8912acfb4a970353fe984a3e1a34704fd
rmwwrt <kalasnikovam403@gmail.com>
2026-02-20 10:22:43 +0700

    feat: setup network layer with Retrofit instance

---
+++ app/src/main/java/com/example/collegeschedule/data/api/ScheduleApi.kt
@@ -0,0 +1,17 @@
+package com.example.collegeschedule.data.api
+import com.example.collegeschedule.data.dto.GroupDto
+import com.example.collegeschedule.data.dto.ScheduleByDateDto
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+interface ScheduleApi {
+    @GET("api/schedule/group/{groupName}")
+    suspend fun getSchedule(
+        @Path("groupName") groupName: String,
+        @Query("start") start: String,
+        @Query("end") end: String
+    ): List<ScheduleByDateDto>
+
+    @GET("api/groups")
+    suspend fun getGroups(): List<GroupDto>
+}
